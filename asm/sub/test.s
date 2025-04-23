971ffd0723b0f90f425ff482d263642569b96a63
rogerg6 <rogerg6@163.com>
2025-04-23 22:29:38 +0800

    [asm] sub

---
+++ asm/sub/test.s
@@ -0,0 +1,13 @@
+
+.text
+.globl _start
+
+_start:
+    li x6, -1                        # x6 = 1
+    li x7, -2                        # x7 = 2
+    sub x5, x6, x7                  # x5 = x6 - x7
+
+stop:
+    j stop
+
+.end                                # end of file
