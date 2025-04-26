103716e335306fd3cfeedb4b183f350b125fe802
rogerg6 <rogerg6@163.com>
2025-04-26 16:03:41 +0800

    [asm] and

---
+++ asm/and/test.s
@@ -0,0 +1,16 @@
+# and rd, rs1, rs2
+# rd = rs1 & rs2
+#
+
+.text
+.globl _start
+
+_start:
+    li x6, 0xf0
+    li x7, 0x0f
+    and x5, x6, x7                  # x5 = x6 & x7
+
+stop:
+    j stop
+
+.end                                # end of file
